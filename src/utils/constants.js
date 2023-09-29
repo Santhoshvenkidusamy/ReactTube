79672659a77c476e74fd9932cefdbef78f0dc422
Santhoshvenkidusamy <santhoshvenkidusamy@gmail.com>
2023-09-29 11:41:56 +0530

    api changes

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -3,7 +3,7 @@ const GOOGLE_API_KEY = 'AIzaSyDjxFu39XuwnhwWRkmhfditSoNT6VD5ULY'
 export const SEARCH_API_KEY = '&key=AIzaSyDjxFu39XuwnhwWRkmhfditSoNT6VD5ULY'
 export const YOUTUBE_API=`https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=IN&maxResults=100&key=${GOOGLE_API_KEY}`;
 
-export const SUGGEST_API ='http://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
+export const SUGGEST_API ='https://suggestqueries.google.com/complete/search?client=firefox&ds=yt&q=';
 
 export const YOUTUBE_SEARCH_API = `https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=25&type=video&q=`;
 
